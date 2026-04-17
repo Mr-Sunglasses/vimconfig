2683e089f781fb4d9d0b32577ef9e8ff21d8fd26
Kanishk Pachauri <itskanishkp.py@gmail.com>
2026-04-18 03:49:19 +0530

    fix: treesitter issue

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,10 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
+return {
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
+  main = "nvim-treesitter.configs",
+  opts = {
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python", "rust", "go", "bash" },
     highlight = { enable = true },
-    indent = { enable = true },  
-    })
-  end
+    indent = { enable = true },
+  },
 }
